c9685cbf567eeaf2b4f3811b4c0a96c8d89ad4a3
jpfang <g.jpfang@gmail.com>
2026-05-27 11:11:01 +0000

    Add genJsconfig symlink to install.sh

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ cp -r "$SCRIPT_DIR/.vim" ~/
 # --- genClangd script ---
 mkdir -p ~/usr/bin
 ln -sf "$SCRIPT_DIR/usr/bin/genClangd" ~/usr/bin/genClangd
+ln -sf "$SCRIPT_DIR/usr/bin/genJsconfig" ~/usr/bin/genJsconfig
 
 # --- Nvim config ---
 mkdir -p ~/.config/nvim
